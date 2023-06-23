f94f8bbb4a14b9014967680db80928219cde60b9
PanganJerome <89790501+PanganJerome@users.noreply.github.com>
2023-06-23 17:04:39 +0800

    updated

---
+++ app/Models/Output.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Output extends Model
+{
+    use HasFactory;
+    protected $table = 'output';
+    protected $fillable = [
+        'task_code',
+        'activity_code',
+        'student_id',
+        'first_name',
+        'last_name',
+        'adviser_id',
+        'title',
+        'description',
+        'due_date',
+        'attachments',
+        'status',
+    ];
+
+        public function student()
+        {
+            return $this->belongsTo(Student::class,'account_code','student_id');
+        }
+
+}
